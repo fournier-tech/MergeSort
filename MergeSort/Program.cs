cdeddc89f65bcc37e08bfc80d04f136370703700
Thomas J. Fournier <tfournier2600@gmail.com>
2022-06-30 12:59:36 -0400

    06/30/2022 - 12:59

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,109 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace InsertionSort {
+﻿namespace InsertionSort {
     class Program {
 
         static void Main(string[] args) {
 
-            int[] integerArray = { 6, 5, 6, 0, 3, 10, 4, 6, 5, 0 };
-            int i = 0;
-            int j = integerArray.Length - 1;
+            //arbitrary list of unsorted integers
+            int[] integerArray = { 6, 5, -6, 0, 3, 10, 4, 6, -5, 0 };
+            int lowIndex = 0;
+            int highIndex = integerArray.Length - 1;
 
             Console.WriteLine("\nUnsorted List of Integers: ");
             PrintList(integerArray);
             Console.WriteLine("\nSorted List of Integers: ");
-            MergeSort(integerArray, i, j);
+            MergeSort(integerArray, lowIndex, highIndex);
             PrintList(integerArray);
             Console.WriteLine("\n");
         }
 
-        // infinity(∞) = alt + 236
-        // less than or equal (≤) = alt + 243
-        //floor function ⌊ ⌋
+        public static void PrintList(int[] integerArray) {
 
-        //Merge-Sort(A, i, k)           //
-        //  if i < k
-        //      j = ⌊ (i + k) / 2 ⌋
-        //      Merge-Sort(A, i, j)
-        //      Merge-Sort(A, j + 1, k)
-        //      Merge(A, i, j, k)
+            foreach (int i in integerArray) {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine('\n');
+        }
 
-        //Merge(A, i, j, k)
-        //  n1 = j - i + 1
-        //  n2 = k - j
-        //  new arrays Left[1... n1 + 1] and Right[1...n2 + 1]
-        //  for l = 1 to n1
-        //      Left[l] = A[i + l - 1]
-        //  for m = 1 to n2
-        //      Right[m] = A[j + m]
-        //  Left[n1 + 1] = ∞
-        //  Right[n2 + 1] = ∞
-        //  l = 1
-        //  m = 1
-        //  for n = i to k
-        //      if Left[l] ≤ Right[m]
-        //          A[n] = Left[l]
-        //          l = l + 1
-        //      else A[n] = Right[m]
-        //          m = m + 1
+        //Merge-Sort(Array[], low, high)
+        //  if low < high
+        //      //round down to find the midpoint
+        //      mid = ⌊(low + high) / 2⌋
+        //      //recurrsive method calls
+        //      Merge-Sort(Array[], low, mid)
+        //      Merge-Sort(Array[], mid + 1, high)
+        //      Merge(Array[], low, mid, high)
 
-        public static void MergeSort(int[] A, int p, int r) {
+        public static void MergeSort(int[] integerArray, int lowIndex, int highIndex) {
 
-            if (p < r) {
-                int q = (p + r) / 2;
-                MergeSort(A, p, q);
-                MergeSort(A, q + 1, r);
-                Merge(A, p, q, r);
+            if (lowIndex < highIndex) {
+                int midIndex = (lowIndex + highIndex) / 2;
+                MergeSort(integerArray, lowIndex, midIndex);
+                MergeSort(integerArray, midIndex + 1, highIndex);
+                Merge(integerArray, lowIndex, midIndex, highIndex);
             }
         }
 
-        public static void Merge(int[] A, int p, int q, int r) {
+        //Merge(Array[], low, mid, high)
+        //  elementsLeft = mid - low + 1
+        //  elementsRight = high - mid
+        //  new array Left[1... elementsLeft + 1]
+        //  new array Right[1...elementsRight + 1]
+        //  for i = 1 to elementsLeft
+        //      Left[i] = Array[low + i - 1]
+        //  for j = 1 to elementsRight
+        //      Right[j] = Array[mid + j]
+        //  Left[elementsLeft + 1] = ∞
+        //  Right[elementsRight + 1] = ∞
+        //  i = 1
+        //  j = 1
+        //  for k = low to high
+        //      if Left[i] ≤ Right[j]
+        //          Array[k] = Left[i]
+        //          i = i + 1
+        //      else Array[k] = Right[j]
+        //          j = j + 1
+
+        public static void Merge(int[] integerArray, int lowIndex, int midIndex, int highIndex) {
 
             int i, j, k;
             //calculate the needed array elements for the left sub array
-            int n1 = q - p + 1;
+            int elementsLeftArray = midIndex - lowIndex + 1;
             //calculate the needed array elements for the right sub array
-            int n2 = r - q;
+            int elementsRightArray = highIndex - midIndex;
             //create left and right aux arrays
-            int[] L = new int[n1 + 1];
-            int[] R = new int[n2 + 1];
+            int[] leftSubArray = new int[elementsLeftArray + 1];
+            int[] rightSubArray = new int[elementsRightArray + 1];
 
-            double zero = 0;
-            double infinity = (1 / zero) - 1;
+            leftSubArray[elementsLeftArray] = int.MaxValue;
+            rightSubArray[elementsRightArray] = int.MaxValue;
 
-            for (i = 1; i < n1; i++) {
-                L[i] = A[p + i - 1];
+            //initialize sub array with first half of original array
+            for (i = 0; i < elementsLeftArray; i++) {
+                leftSubArray[i] = integerArray[lowIndex + i];
             }
 
-            for (j = 1; j < n2; j++) {
-                R[j] = A[q + j];
+            //initialize sub array with second half of original array
+            for (j = 0; j < elementsRightArray; j++) {
+                rightSubArray[j] = integerArray[midIndex + j + 1];
             }
-
-            L[n1 + 1] = (int)infinity;
-            R[n2 + 1] = (int)infinity;
 
             i = 0;
             j = 0;
 
-            for (k = p; k < r; k++) {
-                if (L[i] < R[j]) {
-                    A[k] = L[i];
+            //merge left and right sub arrays into original
+            for (k = lowIndex; k <= highIndex; k++) {
+                if (leftSubArray[i] <= rightSubArray[j]) {
+                    integerArray[k] = leftSubArray[i];
                     i++;
                 } else {
-                    A[k] = R[j];
+                    integerArray[k] = rightSubArray[j];
                     j++;
                 }
-            }
-
-            //return A;
-        }
-
-        public static void PrintList(int[] integerArray) {
-            foreach (int i in integerArray) {
-                Console.Write(i + " ");
             }
         }
     }
